dfde5a57cbc40c51370591bed52c54023345c12d
jalak274 <jalakgevariya274@gmail.com>
2024-05-04 04:48:40 -0400

    Added permission and other code to overcome issue

--- app/src/main/java/com/safe/resident/pro/app/AccountActivity.kt
+++ app/src/main/java/com/safe/resident/pro/app/AccountActivity.kt
@@ -52,7 +52,15 @@ class AccountActivity : AppCompatActivity() {
         loadUserData()
         loadIncidents()
 
+                // Check scoped storage permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (Environment.isExternalStorageManager()) {
+                // Permission granted
 
+            } else {
+                requestScopedStoragePermission()
+            }
+        }
         cameraActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = result.data
@@ -73,6 +81,8 @@ class AccountActivity : AppCompatActivity() {
             .setPositiveButton("OK") { dialog, which ->
                 val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
                 val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
             }
             .setNegativeButton("Cancel") { dialog, which ->
                 dialog.dismiss()
@@ -94,6 +104,8 @@ class AccountActivity : AppCompatActivity() {
                 this,
                 arrayOf(
                     android.Manifest.permission.CAMERA,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
                 ),
                 PERMISSION_REQUEST_CODE
             )
