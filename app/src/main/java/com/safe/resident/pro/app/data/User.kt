bde875a6b6542270b19ca625420caac56bda32a1
jalk274 <jalakgevariya274@gmail.com>
2024-05-04 02:23:22 -0400

    New code for project components and code for camera, storage permission, gallary, trackFragment

---
+++ app/src/main/java/com/safe/resident/pro/app/data/User.kt
@@ -0,0 +1,8 @@
+package com.safe.resident.pro.app.data
+
+data class User(
+    val userid: String = "",
+    val email: String = "",
+    val password: String = "",
+    val profile_pic : String = ""
+)
